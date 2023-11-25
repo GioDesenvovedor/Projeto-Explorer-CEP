e8e953635c9bac1bebb46e90e38003ee3a561ebb
Giovanni <gio.dev.app@gmail.com>
2023-11-25 20:50:56 -0300

    adicionando botoes e mudanca no layout

--- app/src/main/java/com/gvn/cepexplorer/CepResultado.kt
+++ app/src/main/java/com/gvn/cepexplorer/CepResultado.kt
@@ -1,5 +1,7 @@
 package com.gvn.cepexplorer
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,7 +9,9 @@ import com.gvn.cepexplorer.dataSource.EnderecoAPI
 import com.gvn.cepexplorer.dataSource.RetrofitHelper
 import com.gvn.cepexplorer.databinding.ActivityCepResultadoBinding
 import com.gvn.cepexplorer.model.Endereco
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Response
 
@@ -21,12 +25,41 @@ class CepResultado : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //esconder actionBar
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#52057B")
+
         val extras = intent.extras
         binding.txtEndereco.text = extras?.getString("logradouro")
         binding.txtBairro.text = extras?.getString("bairro")
         binding.txtCidade.text = extras?.getString("cidade")
 
+        val cardResultado = binding.card
+        val endereco = binding.txtEndereco.text.toString()
+        val bairro = binding.txtBairro.text.toString()
+        val cidade = binding.txtCidade.text.toString()
+        btnCompartilhar(endereco, bairro, cidade)
+
     }
 
 
+    private fun btnCompartilhar(endereco: String,bairro: String,cidade: String) {
+       binding.btnCompartilhar.setOnClickListener {
+           CoroutineScope(Dispatchers.IO).launch {
+
+               compartilharCep(endereco, bairro, cidade)
+           }
+
+       }
+    }
+
+
+    private fun compartilharCep(logradouro: String, bairro: String, cidade: String){
+
+        val menssagem = "Endereco: $logradouro, Bairro: $bairro, cidade: $cidade"
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, menssagem)
+    }
 }
